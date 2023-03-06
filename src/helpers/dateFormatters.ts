e1961b2ccad5eaaea57436ccad17ac9ebbadc13e
Agnar <105129747+agnar-nomad@users.noreply.github.com>
2023-03-06 10:37:19 +0100

    added some css and helpers to format dates to and from ISO format

---
+++ src/helpers/dateFormatters.ts
@@ -0,0 +1,22 @@
+export function formatToISO8601(dateString: string): string {
+  const parts = dateString.split(' ');
+  const dateParts = parts[0].split('/');
+  const timeParts = parts[1].split(':');
+  const year = parseInt(dateParts[2]);
+  const month = parseInt(dateParts[1]) - 1;
+  const day = parseInt(dateParts[0]);
+  const hours = parseInt(timeParts[0]);
+  const minutes = parseInt(timeParts[1]);
+  const date = new Date(Date.UTC(year, month, day, hours, minutes));
+  return date.toISOString();
+}
+
+export function formatDate(isoDateString: string): string {
+  const date = new Date(isoDateString);
+  const day = date.getUTCDate().toString().padStart(2, '0');
+  const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+  const year = date.getUTCFullYear().toString();
+  const hours = date.getUTCHours().toString().padStart(2, '0');
+  const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+  return `${day}/${month}/${year} ${hours}:${minutes}`;
+}
